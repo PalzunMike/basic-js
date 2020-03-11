18555e9aea977be4b9231ebe1bbee218d1181a74
MikePalzun <palzun@gmail.com>
2020-03-11 15:35:30 +0300

    feat: finish task count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,11 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let catsArr = [];
+  for ( let i=0; i < matrix.length; i++) {
+    for (let j=0; j < matrix[i].legth; j++){
+      if (matrix[i][j] == '^^'){
+        catsArr.push(j);
+      }
+    }
+  }
+  return catsArr.length;
 };
